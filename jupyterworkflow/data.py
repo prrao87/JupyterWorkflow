7558109f577f5c6777b17cc436a54bba8ff6d0a2
Prashanth Rao <prrao87@gmail.com>
2018-09-03 17:24:08 -0700

    Move data download to jupyterworkflow package

---
+++ jupyterworkflow/data.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+from urllib.request import urlretrieve
+
+FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=False):
+    
+    """Download and cache the Fremont data
+    
+    Parameters
+    ----------
+    filename: string (optional)
+        Location to save the data
+    url: strint (optional)
+        Web location of the data
+    force_download: bool (optional)
+        if True, force redownload of the data
+        
+    Returns
+    -------
+    data: pandas.DataFrame
+        Fremont bridge bike data downloaded from seattle.gov website
+    """
+    if force_download or not os.path.exists(filename):
+        urlretrieve(URL, 'Fremont.csv')
+    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    
+    # Rename columns to reduce verbosity
+    data.columns = ['East', 'West']
+    # Calculate total bike rides as a sum of both column entries
+    data['Total'] = data['West'] + data['East']
+
+    return data
